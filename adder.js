00cfcf19f5440249af2020e118fc09e71b41d4cf
mayanksingh2233 <singhmayank.ms123@gmail.com>
2024-10-15 11:14:44 +0530

    update adder function

---
+++ adder.js
@@ -0,0 +1,3 @@
+export const adder = ()=>{
+    return 0
+}
--- test/adder.spec.js
+++ test/adder.spec.js
@@ -1,8 +1,11 @@
 import { assert } from "chai";
+import { adder } from "../adder.js";
+
+// import { adder } from "../adder.js";
 
 describe("adder", () => {
   it("should return 0 for no input", () => {
-    const result = add();
+    const result = adder();
     assert.equal(result, 0);
   });
 
